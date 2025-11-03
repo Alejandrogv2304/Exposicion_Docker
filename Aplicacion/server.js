9124f1c94f1d0289b407ccb48319e356e69dc859
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2025-11-03 14:20:13 -0500

    Mejora aplicación web

--- Aplicacion/server.js
+++ Aplicacion/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Servir archivos estáticos 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal
 app.get('/', (req, res) => {
-  res.send('¡Hola desde mi aplicación Node.js dockerizada en Operativos');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/docker', (req, res) => {
+  res.send('Hola desde la aplicación dockerizada Grupo 2');
 });
 
 app.listen(PORT, () => {
